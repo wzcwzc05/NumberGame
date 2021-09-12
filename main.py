65dd4df827bc9d64f8f4c182df1fa1e42275cca2
UnderStarlight <wzcwzc0@outlook.com>
2021-09-12 17:04:07 +0800

    Update

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ class untitled(App):
         container0 = Container()
         container0.attr_class = "Container"
         container0.attr_editor_newclass = False
-        container0.css_height = "285.0px"
+        container0.css_height = "270.0px"
         container0.css_left = "30.0px"
         container0.css_position = "absolute"
         container0.css_top = "45.0px"
-        container0.css_width = "420.0px"
+        container0.css_width = "405.0px"
         container0.variable_name = "container0"
         container1 = Container()
         container1.attr_class = "Container"
@@ -43,24 +43,26 @@ class untitled(App):
         container2 = Container()
         container2.attr_class = "Container"
         container2.attr_editor_newclass = False
-        container2.css_height = "250px"
+        container2.css_height = "270.0px"
         container2.css_left = "510.0px"
         container2.css_position = "absolute"
         container2.css_top = "-15.0px"
-        container2.css_width = "250px"
+        container2.css_visibility = "visible"
+        container2.css_width = "270.0px"
         container2.variable_name = "container2"
         container0.append(container2,'container2')
         button0 = Button()
         button0.attr_class = "Button"
         button0.attr_editor_newclass = False
         button0.css_height = "30px"
-        button0.css_left = "150.0px"
+        button0.css_left = "90.0px"
         button0.css_position = "absolute"
         button0.css_top = "180.0px"
         button0.css_width = "100px"
         button0.text = "Login"
         button0.variable_name = "button0"
         container0.append(button0,'button0')
+        global label0
         label0 = Label()
         label0.attr_class = "Label"
         label0.attr_editor_newclass = False
@@ -72,6 +74,7 @@ class untitled(App):
         label0.text = "User:"
         label0.variable_name = "label0"
         container0.append(label0,'label0')
+        global label1
         label1 = Label()
         label1.attr_class = "Label"
         label1.attr_editor_newclass = False
@@ -83,23 +86,28 @@ class untitled(App):
         label1.text = "Password:"
         label1.variable_name = "label1"
         container0.append(label1,'label1')
+        global textinput0
         textinput0 = TextInput()
         textinput0.attr_class = "TextInput"
         textinput0.attr_editor_newclass = False
-        textinput0.attr_maxlength = "0"
+        textinput0.attr_maxlength = "200"
+        textinput0.css_font_size = "15px"
         textinput0.css_font_style = "normal"
         textinput0.css_height = "20px"
         textinput0.css_left = "135px"
         textinput0.css_position = "absolute"
         textinput0.css_top = "105.0px"
         textinput0.css_width = "150px"
+        textinput0.css_writing_mode = "none"
         textinput0.text = ""
         textinput0.variable_name = "textinput0"
         container0.append(textinput0,'textinput0')
+        global textinput1
         textinput1 = TextInput()
         textinput1.attr_class = "TextInput"
         textinput1.attr_editor_newclass = False
-        textinput1.attr_maxlength = "0"
+        textinput1.attr_maxlength = "200"
+        textinput1.css_font_size = "15px"
         textinput1.css_height = "20px"
         textinput1.css_left = "135.0px"
         textinput1.css_position = "absolute"
@@ -108,6 +116,7 @@ class untitled(App):
         textinput1.text = ""
         textinput1.variable_name = "textinput1"
         container0.append(textinput1,'textinput1')
+        global label2
         label2 = Label()
         label2.attr_class = "Label"
         label2.attr_editor_newclass = False
@@ -120,11 +129,32 @@ class untitled(App):
         label2.text = "Login"
         label2.variable_name = "label2"
         container0.append(label2,'label2')
+        global button1
+        button1 = Button()
+        button1.attr_class = "Button"
+        button1.attr_editor_newclass = False
+        button1.css_height = "30px"
+        button1.css_left = "225.0px"
+        button1.css_position = "absolute"
+        button1.css_top = "180.0px"
+        button1.css_width = "100px"
+        button1.text = "Sign Up"
+        button1.variable_name = "button1"
+        container0.append(button1,'button1')
+        button0.onclick.do(self.onclick_button0)
         
 
         self.container0 = container0
         return self.container0
     
+    def onclick_button0(self, emitter):
+        Users = textinput0.text
+        PassWord = textinput1.text
+        print(Users,PassWord)
+
+    def onclick_button1(self, emitter):
+        pass
+
 
 
 #Configuration
